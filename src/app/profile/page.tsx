17bdaa185dba827cb69f28a10d1a0271f4ad88e0
Mohsin <mohsin@sg-m-70.softlink.local>
2024-02-07 13:01:40 +0530

    added auth middleware

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,4 +1,22 @@
-export default function ProfilePage(){
+"use client"
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+export default function ProfilePage() {
+
+    const router = useRouter();
+    const logout = async () => {
+        try {
+
+            await axios.get("/api/users/logout")
+
+            router.push('/login')
+        } catch (error) {
+            console.log("failed to logout", error);
+
+        }
+    }
+
     return (
         <div
             className="flex flex-col items-center justify-center min-h-screen py-2"
@@ -6,6 +24,13 @@ export default function ProfilePage(){
             <h1>Profile</h1>
             <hr />
             <p>Profile page</p>
+            <hr />
+
+            <button onClick={logout}
+                className="bg-blue-500 hover:bg-blue-700 text-white
+            font-bold py-2 px-4 rounded">
+                Logout
+            </button>
         </div>
     )
 }
