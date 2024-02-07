24f77f3ab2721a2be10c53ab98fe54f9b860134e
Mohsin <mohsin@sg-m-70.softlink.local>
2024-02-07 18:28:40 +0530

    added me route

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,6 +2,7 @@
 import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 export default function ProfilePage() {
 
     const router = useRouter();
@@ -17,6 +18,14 @@ export default function ProfilePage() {
         }
     }
 
+    const [data, setData] = useState("");
+
+    const getUserDetails = async ()=>{
+        const res = await axios.get('/api/users/me')
+        console.log(res.data);
+        setData(res.data.data._id)
+    }
+
     return (
         <div
             className="flex flex-col items-center justify-center min-h-screen py-2"
@@ -24,12 +33,23 @@ export default function ProfilePage() {
             <h1>Profile</h1>
             <hr />
             <p>Profile page</p>
+            <h2 className="p-1 rounded bg-green-500">{data==="" ? "No data to show":
+            <Link href={`/profile/${data}`}>
+                {data}</Link>}</h2>
             <hr />
+
+            
 
             <button onClick={logout}
                 className="bg-blue-500 hover:bg-blue-700 text-white
             font-bold py-2 px-4 rounded">
                 Logout
+            </button>
+
+            <button onClick={getUserDetails}
+                className="bg-green-700 mt-4 hover:bg-blue-700 text-white
+            font-bold py-2 px-4 rounded">
+                Get User details
             </button>
         </div>
     )
